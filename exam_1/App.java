94138fc5f3f683311ece5458cdc176242f9f88a1
Aljean Kervy Pastrana <111488368+Jhynz@users.noreply.github.com>
2022-09-08 14:03:48 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student Jhynz = new Student ("Pastrana", "Aljean Kervy", 'P', "07/02/2004", 2022101382, "pastranaap@students.national-u.edu.ph", true );
+        Jhynz.sayMyStudentNumber();
+        Jhynz.sayMyEmailAddress();
+        Jhynz.sayIAmAwesome();
+        
+    }
